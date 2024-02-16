1d57d58c7e6c85fe5f6a58433a1250a51881d44d
KadryDAV <alialkadryofficial@gmail.com>
2024-02-15 16:44:46 -0800

    completed exercise

---
+++ restAndSpread.js
@@ -0,0 +1,54 @@
+//es5
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+
+//es2015
+const filterOutOdds = (...args) => args.filter(v => v % 2 === 0)
+
+//findMin
+const findMin = (...args) => Math.min(...args)
+
+//mergeObjects
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
+
+//doubleAndReturnArgs
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v * 2)]
+
+//Slice and Dice
+
+const removeRandom = items => {
+    let idx = Math.floor(Math.random() * items.length)
+    return [...items.slice(0, idx), ...items.slice(idx + 1)]
+  }
+  
+  const extend = (array1, array2) => {
+    return [...array1, ...array2]
+  }
+  
+   * from obj and a new key/value pair */
+  const addKeyVal = (obj, key, val) => {
+    let newObj = { ...obj }
+    newObj[key] = val
+    return newObj
+  }
+  
+  
+  const removeKey = (obj, key) => {
+    let newObj = { ...obj }
+    delete newObj[key]
+    return newObj
+  }
+  
+  const combine = (obj1, obj2) => {
+    return { ...obj1, ...obj2 }
+  }
+  
+  const update = (obj, key, val) => {
+    let newObj = { ...obj }
+    newObj[key] = val
+    return newObj
+  }
